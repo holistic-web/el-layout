4f481fbe6acbf4963cc53e5954140b1fb0f7ce07
Charlie Lancaster <charlie.lancaster@yousoft.com>
2020-03-18 14:32:28 +0000

    Add form and elements to components

---
+++ src/components/form-components/ElFormSelect.vue
@@ -0,0 +1,29 @@
+<template>
+	<b-form-input
+	:value="value"
+	:options="options"
+	@input="update($event.target.value)" />
+</template>
+
+<script>
+import { FormMixin } from 'formvuelatte';
+
+export default {
+	mixins: [FormMixin],
+	props: {
+		value: {
+			type: String,
+			required: true
+		},
+		options: {
+			type: Array,
+			required: true
+		}
+	},
+	methods: {
+		update(data) {
+			this.$emit('input', data);
+		}
+	}
+};
+</script>
